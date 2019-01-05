55f4a4398b4a06163a785e9e07de16077d4ac8ca
Gennaro Imperatore <gennaro.imperatore@geo-sport.co.uk>
2019-01-05 07:56:23 -0800

    fixed gradle configuration

--- app/src/main/java/net/crosp/customradiobtton/ResultsActivity.java
+++ app/src/main/java/net/crosp/customradiobtton/ResultsActivity.java
@@ -1,14 +1,24 @@
 package net.crosp.customradiobtton;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    Button mPrevButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+    }
+
+    public void GoToMain()
+    {
+        Intent myIntent = new Intent(this, ResultsActivity.class);
+        startActivity(myIntent);
     }
 }
