50ef25a51a7f828de829764716b84285f83891ae
Gennaro Imperatore <gennaro.imperatore@geo-sport.co.uk>
2019-01-05 08:31:44 -0800

    prev button now works

--- app/src/main/java/net/crosp/customradiobtton/ResultsActivity.java
+++ app/src/main/java/net/crosp/customradiobtton/ResultsActivity.java
@@ -3,6 +3,7 @@ package net.crosp.customradiobtton;
 import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
+import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 
 public class ResultsActivity extends AppCompatActivity {
@@ -14,11 +15,20 @@ public class ResultsActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        mPrevButton =(Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                GoToMain();
+
+            }
+        });
     }
 
     public void GoToMain()
     {
-        Intent myIntent = new Intent(this, ResultsActivity.class);
+        Intent myIntent = new Intent(this, MainActivity.class);
         startActivity(myIntent);
     }
 }
