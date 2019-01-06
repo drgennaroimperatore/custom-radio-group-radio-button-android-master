b51d62ca4bebca8daa11ce5479f19f1454d25a0f
Gennaro Imperatore <gennaro.imperatore@geo-sport.co.uk>
2019-01-06 07:07:21 -0800

    added initial pie chart

--- app/src/main/java/net/crosp/customradiobtton/ResultsActivity.java
+++ app/src/main/java/net/crosp/customradiobtton/ResultsActivity.java
@@ -4,11 +4,48 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.graphics.*;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.MotionEvent;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.androidplot.pie.PieChart;
+import com.androidplot.pie.PieRenderer;
+import com.androidplot.pie.Segment;
+import com.androidplot.pie.SegmentFormatter;
+import com.androidplot.util.*;
+import com.androidplot.ui.DynamicTableModel;
+import com.androidplot.ui.FixedTableModel;
+import com.androidplot.ui.TableOrder;
+import com.androidplot.ui.Size;
+import com.androidplot.ui.SizeMode;
+import com.androidplot.ui.HorizontalPositioning;
+import com.androidplot.ui.VerticalPositioning;
+
+
 import android.support.v7.app.AppCompatActivity;
+
+
+
 
 public class ResultsActivity extends AppCompatActivity {
 
     Button mPrevButton;
+
+    public static final int SELECTED_SEGMENT_OFFSET = 150;
+
+    private TextView donutSizeTextView;
+    private SeekBar donutSizeSeekBar;
+
+    public PieChart pie;
+
+    private Segment s1;
+    private Segment s2;
+    private Segment s3;
+    private Segment s4;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -24,6 +61,66 @@ public class ResultsActivity extends AppCompatActivity {
 
             }
         });
+
+        // initialize our XYPlot reference:
+        pie = (PieChart) findViewById(R.id.mySimplePieChart);
+
+        // enable the legend:
+        pie.getLegend().setVisible(true);
+        pie.getLegend().setTableModel(new DynamicTableModel(1,4, TableOrder.ROW_MAJOR));
+        pie.getLegend().setSize(new Size(
+                PixelUtils.dpToPix(90), SizeMode.ABSOLUTE,
+                PixelUtils.dpToPix(70), SizeMode.ABSOLUTE));
+        pie.getLegend().position(
+                0.5f, HorizontalPositioning.RELATIVE_TO_LEFT,
+                0.1f, VerticalPositioning.RELATIVE_TO_TOP);
+
+        final float padding = PixelUtils.dpToPix(25);
+        pie.getPie().setPadding(padding, padding, padding, padding);
+
+        pie.getPie().setSize(new Size(
+                PixelUtils.dpToPix(170), SizeMode.ABSOLUTE,
+                PixelUtils.dpToPix(150), SizeMode.ABSOLUTE));
+        pie.getPie().position(
+                0, HorizontalPositioning.RELATIVE_TO_LEFT,
+                0, VerticalPositioning.RELATIVE_TO_TOP);
+
+       // donutSizeTextView = (TextView) findViewById(R.id.donutSizeTextView);
+        //updateDonutText();
+
+
+
+        s1 = new Segment("30%", 60);
+        s2 = new Segment("10%", 15);
+        s3 = new Segment("70%", 12);
+        s4 = new Segment("90%", 12);
+
+        EmbossMaskFilter emf = new EmbossMaskFilter(
+                new float[]{1, 1, 1}, 0.4f, 10, 8.2f);
+
+        SegmentFormatter sf1 = new SegmentFormatter(this, R.xml.pie_segment_formatter1);
+        sf1.getLabelPaint().setShadowLayer(3, 0, 0, Color.BLACK);
+        sf1.getFillPaint().setMaskFilter(emf);
+
+        SegmentFormatter sf2 = new SegmentFormatter(this, R.xml.pie_segment_formatter2);
+        sf2.getLabelPaint().setShadowLayer(3, 0, 0, Color.BLACK);
+        sf2.getFillPaint().setMaskFilter(emf);
+
+        SegmentFormatter sf3 = new SegmentFormatter(this, R.xml.pie_segment_formatter3);
+        sf3.getLabelPaint().setShadowLayer(3, 0, 0, Color.BLACK);
+        sf3.getFillPaint().setMaskFilter(emf);
+
+        SegmentFormatter sf4 = new SegmentFormatter(this, R.xml.pie_segment_formatter4);
+        sf4.getLabelPaint().setShadowLayer(3, 0, 0, Color.BLACK);
+        sf4.getFillPaint().setMaskFilter(emf);
+
+        pie.addSegment(s1, sf1);
+        pie.addSegment(s2, sf2);
+        pie.addSegment(s3, sf3);
+        pie.addSegment(s4, sf4);
+
+        pie.getBorderPaint().setColor(Color.TRANSPARENT);
+        pie.getBackgroundPaint().setColor(Color.TRANSPARENT);
     }
 
     public void GoToMain()
@@ -31,4 +128,6 @@ public class ResultsActivity extends AppCompatActivity {
         Intent myIntent = new Intent(this, MainActivity.class);
         startActivity(myIntent);
     }
+
+
 }
